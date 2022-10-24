087f2566f35843908317fd6acba99431791a5e4f
byounis <byounis0@gmail.com>
2022-10-24 08:01:25 -0600

    Created dialogue editor window that opens when you select a dialogue in the project window.

---
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace RPG.Dialogue.Editor
+{
+    public class DialogueEditor : EditorWindow
+    {
+        [MenuItem("Window/Dialogue Editor")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<DialogueEditor>();
+            window.titleContent = new GUIContent("Dialogue Editor");
+            window.Show();
+        }
+
+        [OnOpenAsset(1)]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            var dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+
+            if (dialogue != null)
+            {
+                ShowWindow();
+                return true;
+            }
+            
+            return false;
+        }
+
+        private void OnGUI()
+        {
+            
+        }
+    }
+}

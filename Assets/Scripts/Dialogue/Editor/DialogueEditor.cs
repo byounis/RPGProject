b01190835b12f74de595e838c8f3e8d21c7e79c8
byounis <byounis0@gmail.com>
2022-10-24 08:25:59 -0600

    Added selection of dialogue in the dialogue editor.

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -6,6 +6,8 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        private Dialogue _selectedDialogue = null;
+        
         [MenuItem("Window/Dialogue Editor")]
         private static void ShowWindow()
         {
@@ -28,9 +30,41 @@ namespace RPG.Dialogue.Editor
             return false;
         }
 
+        private void OnEnable()
+        {
+            Selection.selectionChanged += SelectionChanged;
+        }
+        
+        private void OnDisable()
+        {
+            Selection.selectionChanged -= SelectionChanged;
+        }
+
+        private void SelectionChanged()
+        {
+            var activeDialogue = Selection.activeObject as Dialogue;
+            if (activeDialogue != null)
+            {
+                _selectedDialogue = activeDialogue;
+            }
+            else
+            {
+                _selectedDialogue = null;
+            }
+            Repaint();
+        }
+
         private void OnGUI()
         {
-            
+            if (_selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected");
+                
+            }
+            else
+            {
+                EditorGUILayout.LabelField(_selectedDialogue.name);
+            }
         }
     }
 }

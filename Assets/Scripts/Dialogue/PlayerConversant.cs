6d41c00afa5805f7bdd5ee1df0afca912c793e25
byounis <byounis0@gmail.com>
2022-11-02 07:45:23 -0600

    Added dialogue progression using the next button

--- Assets/Scripts/Dialogue/PlayerConversant.cs
+++ Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,11 +1,20 @@
 using System;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace RPG.Dialogue
 {
     public class PlayerConversant : MonoBehaviour
     {
         [SerializeField] private Dialogue _currentDialogue;
+
+        private DialogueNode _currentNode;
+
+        private void Awake()
+        {
+            _currentNode = _currentDialogue.GetRootNode();
+        }
 
         public string GetText()
         {
@@ -14,7 +23,18 @@ namespace RPG.Dialogue
                 return string.Empty;
             }
             
-            return _currentDialogue.GetRootNode().GetText();
+            return _currentNode.GetText();
+        }
+
+        public void Next()
+        {
+            var childNodes = _currentDialogue.GetAllChildren(_currentNode).ToArray();
+            _currentNode = childNodes[Random.Range(0, childNodes.Length)];
+        }
+
+        public bool HasNext()
+        {
+            return _currentNode.GetChildren().Count > 0;
         }
     }
 }
--- Assets/Scripts/UI/DialogueUI.cs
+++ Assets/Scripts/UI/DialogueUI.cs
@@ -2,19 +2,34 @@ using System;
 using UnityEngine;
 using RPG.Dialogue;
 using TMPro;
+using UnityEngine.UI;
 
 namespace RPG.UI
 {
     public class DialogueUI : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _aiText;
+        [SerializeField] private Button _nextButton;
         
         private PlayerConversant _playerConversant;
 
         private void Start()
         {
             _playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            _nextButton.onClick.AddListener(Next);
+            UpdateUI();
+        }
+
+        private void Next()
+        {
+            _playerConversant.Next();
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
             _aiText.SetText(_playerConversant.GetText());
+            _nextButton.gameObject.SetActive(_playerConversant.HasNext());
         }
     }
 }
